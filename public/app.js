c2f74a5be8a0dbd989dea3e7b8c30a381635c67c
eg400 <edward.d.gibson@gmail.com>
2015-12-12 21:47:38 -0800

    rsvp

--- public/app.js
+++ public/app.js
@@ -7,21 +7,178 @@ app.config(['$routeProvider',
         templateUrl: 'html/register.html',
         controller: 'registerController'
       }).
+      when('/rsvp', {
+        templateUrl: 'html/rsvp.html',
+        controller: 'rsvpController'
+      }).
+      when('/rsvp_complete', {
+        templateUrl: 'html/rsvp_complete.html',
+        controller: 'rsvpController'
+      }).
       otherwise({
         redirectTo: '/'
       });
 }]);
 
-app.controller("registerController", ['$scope', '$location', function($scope,$location) {
-  $scope.register = function(){
-      $location.path("/");
-  };
-  //var ref = new Firebase("https://judward2016.firebaseio.com");
+app.controller("judward2016Ctrl", ['$scope', function($scope) {
 }]);
 
-app.controller("judward2016Ctrl", ['$scope', function($scope) {
-  var ref = new Firebase("https://judward2016.firebaseio.com");
+app.controller('rsvpController', ['$scope', '$location','$timeout','userService', function($scope,$location,$timeout,userService) {
+    var ref = new Firebase("https://judward2016.firebaseio.com");
+    $scope.whoAmI = "nobody";
+    $scope.emailService = userService;
+
+
+    $scope.hasPlus1 = 0;
+    $scope.submitted = false;
+    $scope.revealPlus1 = function() {
+        $scope.hasPlus1 = 1;
+    };
+    $scope.hidePlus1 = function() {
+        $scope.hasPlus1 = 0;
+    };
+
+    $scope.go = function(path) {
+        $location.path(path);
+    };
+
+    $scope.register = function(user){
+        //tell the alert boxes form is submitted
+        $scope.submitted = true;
+        //emit event for validity check on form
+        $scope.$broadcast('show-errors-check-validity');
+        //save form if valid
+        if ($scope.rsvpForm.$valid) {
+            console.log("form valid");
+            ref.child(user.self.firstname+"_"+user.self.lastname).child("self").set({
+                email:user.email,
+                firstName: user.self.firstname,
+                lastName: user.self.lastname,
+                starter: user.self.starter,
+                main: user.self.main,
+                marriot: user.marriott,
+                hasPlus1:user.hasPlus1
+            });
+            if(user.hasPlus1==1) {
+                ref.child(user.self.firstname + "_" + user.self.lastname).child("plus1").set({
+                    firstName: user.plus1.firstname,
+                    lastName: user.plus1.lastname,
+                    starter: user.plus1.starter,
+                    main: user.plus1.main
+                });
+            }
+            $scope.emailService.model.email = user.email;
+            $scope.whoAmI = user.email;
+            console.log($scope.whoAmI);
+            $location.path('/rsvp_complete');
+        }
+        else {
+            console.log("form NOT valid");
+        }
+    };
 }]);
+
+
+app.factory('userService', ['$rootScope', function ($rootScope) {
+
+    var service = {
+
+        model: {
+            name: '',
+            email: ''
+        },
+
+        SaveState: function () {
+            sessionStorage.userService = angular.toJson(service.model);
+        },
+
+        RestoreState: function () {
+            service.model = angular.fromJson(sessionStorage.userService);
+        }
+    }
+
+    $rootScope.$on("savestate", service.SaveState);
+    $rootScope.$on("restorestate", service.RestoreState);
+
+    return service;
+}]);
+
+app.controller("rsvpCompleteController", ['$scope', function($scope) {
+
+    $scope.$watch('whoAmI', function() {
+        console.log($scope.whoAmI);
+    });
+
+}]);
+
+app.directive('showErrors', function($timeout) {
+    return {
+        restrict: 'A',
+        require: '^form',
+        link: function (scope, el, attrs, formCtrl) {
+            // find the text box element, which has the 'name' attribute
+            var inputEl   = el[0].querySelector("[name]");
+            // convert the native text box element to an angular element
+            var inputNgEl = angular.element(inputEl);
+            // get the name on the text box
+            var inputName = inputNgEl.attr('name');
+
+            // only apply the has-error class after the user leaves the text box
+            var blurred = false;
+            inputNgEl.bind('blur', function() {
+                blurred = true;
+                el.toggleClass('has-error', formCtrl[inputName].$invalid);
+            });
+
+            scope.$watch(function() {
+                return formCtrl[inputName].$invalid
+            }, function(invalid) {
+                // we only want to toggle the has-error class after the blur
+                // event or if the control becomes valid
+                if (!blurred && invalid) { return }
+                el.toggleClass('has-error', invalid);
+            });
+
+            scope.$on('show-errors-check-validity', function() {
+                el.toggleClass('has-error', formCtrl[inputName].$invalid);
+            });
+
+            scope.$on('show-errors-reset', function() {
+                $timeout(function() {
+                    el.removeClass('has-error');
+                }, 0, false);
+            });
+        }
+    }
+});
+
+
+app.directive('radioErrors', function($timeout) {
+    return {
+        restrict: 'A',
+        require: '^form',
+        link: function (scope, el, attrs, formCtrl) {
+            // find the text box element, which has the 'name' attribute
+            var inputEl   = el[0].querySelector("[name]");
+            // convert the native text box element to an angular element
+            var inputNgEl = angular.element(inputEl);
+            // get the name on the text box
+            var inputName = inputNgEl.attr('name');
+
+            scope.$watch(function() {
+                return formCtrl[inputName].$invalid
+            }, function(invalid) {
+                // we only want to toggle the has-error class if the control becomes valid
+                if (invalid) { return }
+                el.toggleClass('has-error', invalid);
+            });
+
+            scope.$on('show-errors-check-validity', function() {
+                el.toggleClass('has-error', formCtrl[inputName].$invalid);
+            });
+        }
+    }
+});
 
 
 app.controller("loginCtrl", ['$scope', '$timeout', function($scope, $timeout) {
@@ -105,20 +262,3 @@ app.controller("loginCtrl", ['$scope', '$timeout', function($scope, $timeout) {
 
 }]);
 
-app.directive('notification', ['$timeout', function ($timeout) {
-  return {
-    restrict: 'E',
-    template:"<div class='alert alert-{{alertData.type}}' ng-show='alertData.message' role='alert' data-notification='{{alertData.status}}'>{{alertData.message}}</div>",
-    scope:{
-      alertData:"="
-      }
-  };
-}]);
-
-app.controller('MyController', ['$scope', function($scope) {
-  $scope.username = 'World';
-
-  $scope.sayHello = function() {
-    $scope.greeting = 'Hello ' + $scope.username + '!';
-  };
-}]);
